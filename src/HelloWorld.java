b6c2a35177537dc9ae4ba073f5fdfcd4b41755db
“Kenneth <Pittman.Kenny24@gmail.com>
2022-06-06 12:10:18 -0500

    syntax types variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int myFavoriteNumber = 24;
+
+        System.out.println(myFavoriteNumber);
         System.out.println("Hello World");
+
+//        String myString = "3.14159";
+//
+//        System.out.println(myString);
+//
+//        long myNumber = "3.14;
+//        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
+
     }
 }
+
