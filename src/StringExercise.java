016eeb540cd40d27133ed66c421f29770cadc94b
“Kenneth <Pittman.Kenny24@gmail.com>
2022-06-08 17:00:50 -0500

    completed Strings exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String pinkFloyd = "We don't need no education\n" + "We don't need no thought control";
+        System.out.println(pinkFloyd);
+
+        String quotes = "Check \"this\" out!, \"s insdie of \"s";
+        System.out.println(quotes);
+
+        String backSlash = "In windows, the main drive is usually C:\\";
+        System.out.println(backSlash);
+
+        String backSlash2 = "I can do backslashes \\, double backslashes \\" + "\\,\n and the amazing backslash\\" + "\\" + "\\" + "!";
+        System.out.println(backSlash2);
+    }
+}
+
