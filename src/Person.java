b7ca7818d8a201d711106427f9140137e7f14680
“Kenneth <Pittman.Kenny24@gmail.com>
2022-06-12 23:00:52 -0500

    updated Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,25 @@
 public class Person {
+    private String name;
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public Person(String newName) {
+        name = newName;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.printf("Hello %s%n", name);
+    }
+    public static void main(String[] args) {
+        Person p1 = new Person("Kenny");
+        p1.sayHello();
+    }
+
 }
+
