4d38351f6d636e9b81897ab4dd1768f20a266296
“Kenneth <Pittman.Kenny24@gmail.com>
2022-06-15 23:43:41 -0500

    HighLow

---
+++ src/practice.java
@@ -0,0 +1,7 @@
+public class Tree {
+    public static void main(String[] args) {
+        // code for method goes here
+        System.out.println("Hey there, I'm Kenny and I'm learning code in Java!");
+
+    }
+}
